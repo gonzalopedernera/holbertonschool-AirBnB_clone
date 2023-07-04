066ace28ac8732a2ec3bb141fbce8239683c3771
4mmZ <andresmaxi111014@gmail.com>
2023-07-04 14:45:27 -0700

    testing the checker

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,20 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel():
     """ Class that defines all common attributes/methods for other classes """
     def __init__(self):
         self.created_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         update = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         create = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
